6810a1c99bb1eeb22fb78fd6089cb27887c5a4af
luan-nee <luandelsol54@gmail.com>
2023-12-05 14:42:08 -0500

    haciendo moficaciones para la interfaz del perfil de usuario.

--- php/cuenta-usuario/perfil.php
+++ php/cuenta-usuario/perfil.php
@@ -1,17 +1,15 @@
 <?php include("../reutilizable/configuraciones.php"); ?>
     <title>Perfil-User</title>
-    <!-- <link rel="stylesheet" href="<?php echo $list_css['perfil'];?>"> -->
+    <link rel="stylesheet" href="<?php echo $list_css['perfil'];?>">
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit dolorem facere veniam nam sunt ratione neque, id fuga accusamus quasi repellendus ex officiis voluptatem modi eligendi architecto voluptate ipsam magnam. Laborum, inventore et minima quibusdam aliquam quaerat obcaecati repudiandae reprehenderit repellendus, nam vero doloremque in, blanditiis eligendi qui aut necessitatibus? Explicabo eum voluptatem illum cum deleniti, laboriosam alias recusandae libero sapiente repudiandae placeat reprehenderit temporibus earum? Natus deserunt ipsa rem aspernatur aperiam eaque quis nulla explicabo non facilis? Rem quam voluptates quibusdam fuga vitae quis voluptatem laborum, magnam ut rerum esse quasi quae id ipsa suscipit ab impedit necessitatibus! Minus laudantium quo doloribus. Enim, nemo. Rerum laborum animi consequatur perferendis debitis, nam esse sequi ullam consequuntur dolorum quis, eligendi quod recusandae tenetur nisi iure hic maiores! Amet facilis saepe reprehenderit consequatur. Facilis unde totam voluptates. Facilis et aut laboriosam fugit sequi porro, suscipit quod possimus. Quasi nostrum sequi eaque aperiam reprehenderit voluptates dolorum accusantium praesentium a nulla, necessitatibus fugiat. Iste, debitis sit recusandae accusamus dolores, fugit esse eum unde inventore veritatis vitae nisi deleniti porro, temporibus nesciunt facilis dolore! Vitae esse laborum ea quo adipisci. Dignissimos porro illo deleniti ipsam adipisci alias vitae tenetur natus nostrum, praesentium iusto sed quam quod optio aperiam et ab consequatur odio labore. Cupiditate, architecto voluptatum, dignissimos maiores fugit vero consequuntur consequatur quidem aliquid ab commodi sit delectus dolorum modi incidunt? Sunt dicta aperiam nulla facere labore excepturi minus nesciunt rerum quasi. Maiores facilis quis, expedita consequatur alias possimus minima nihil odit optio, enim delectus repellendus vel suscipit eaque facere quaerat accusantium exercitationem porro reprehenderit. Facilis soluta ducimus quibusdam veritatis aliquid. Omnis voluptate, sequi quaerat dicta necessitatibus perspiciatis autem, porro doloribus laboriosam suscipit voluptatum nesciunt adipisci, corporis est quasi. Incidunt natus, sit a numquam tempore corrupti similique quisquam rem recusandae architecto perspiciatis vitae enim eius optio velit ad exercitationem delectus ea. Aliquid, nulla. Delectus dicta aperiam officiis! Corrupti quam magni facere deserunt explicabo praesentium expedita ipsa, ea tempora veritatis sit esse tempore eaque doloribus, animi, ex repudiandae placeat! Voluptatum eum voluptates accusantium in quibusdam beatae repellendus recusandae, saepe tempora enim illum molestiae consequatur eius quidem explicabo libero unde, mollitia aperiam exercitationem, autem quaerat laudantium? Voluptatibus mollitia fugit tempora fugiat repellendus velit ipsa quibusdam ratione veniam error? Culpa voluptatibus exercitationem numquam deleniti? Saepe maxime nostrum nemo voluptatibus voluptates cupiditate obcaecati earum sapiente laudantium consequuntur illo eveniet praesentium ipsam alias dignissimos tenetur ab et, dolores debitis commodi?</p>
-    <h1>hola del sol</h1>
-    <?php//include("../reutilizable/header.php"); ?>
+    <?php include("../reutilizable/header.php"); ?>
     <section class="conteiner-All">
-        <section class="sub-head">
+        <section class="sub-head red">
             <div class="conteiner-img">
                 <img src="../../img/img_user.png" alt="#">
             </div>
-            <article class="datos-head">
+            <article class="datos_sub-head">
                 <!-- los estilos asignados no son de importancia -->
                 <h3 class="--numProduc">12345</h3>
                 <h3 class="--numSegui">54321</h3>
@@ -19,13 +17,13 @@
                 <p>Seguidores</p>
             </article>
         </section>
-        <section class="conteiner-nav">
+        <section class="blue">
             <nav>
                 <a href="#">Productos</a>
                 <a href="#">Otros</a>
             </nav>
         </section>
-        <section class="informacion-user">
+        <section class="informacion-user black">
             <!-- inicio de la estructura de los productos -->
             <article>
                 <!-- img momentanea -->
--- php/reutilizable/header.php
+++ php/reutilizable/header.php
@@ -1,8 +1,13 @@
-<section>
-    <nav>
+<section class="header_section">
+    <nav class="header_nav">
         <button>icono</button>
+        <div class="header_conteiner-puntos">
+            <div class="punto"></div>
+            <div class="punto"></div>
+            <div class="punto"></div>
+        </div>
     </nav>
-    <h1 class="title_header">
-        Nombre del lugar
-    </h1>
 </section>
+<h1 class="header_title">
+    Nombre del lugar
+</h1>
