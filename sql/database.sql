f71043dcd0afc0f68730362facd2eb16c3c63ab1
luan del sol <luandelsol54@gmail.com>
2023-12-19 05:28:23 -0500

    agregando cambios de base de datos

--- sql/database.sql
+++ sql/database.sql
@@ -3,19 +3,25 @@ use ciudad;
 create table user(
 	id int auto_increment not null,
     nombre varchar(60) not null,
-    email varchar(45) not null,
-    celular varchar(9) not null,
+    email varchar(45) unique not null,
+    celular varchar(9) unique not null,
     password varchar(20) not null,
     seguidores int,
     PRIMARY KEY (id)
 );
+DROP table user;
+SELECT * FROM user;
+SELECT * FROM user where email = 'luandelsol54@gmail.com';
 
 create table producto(
 	id int auto_increment not null,
+    id_user int not null,
     nombre varchar(50) not null,
     precio float not null,
+    descripcion text(100) not null,
     foto longblob not null,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) references user (id)
 );
 
 create table user_producto(
@@ -27,3 +33,18 @@ create table user_producto(
     FOREIGN KEY (id_user) references user (id),
     FOREIGN KEY (id_producto) references producto (id)
 );
+
+DROP TABLE user;
+DROP TABLE user_producto;
+DROP TABLE producto;
+    
+INSERT INTO producto (nombre, precio, descripcion, foto) VALUES 
+('', ,'',''),
+(),
+(),
+(),
+();
+
+SELECT * FROM user;
+SELECT * FROM producto;
+
