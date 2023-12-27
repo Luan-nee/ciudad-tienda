78678f3aa6f5f77357c99414ee01c17c1b8dcd57
luan del sol <luandelsol54@gmail.com>
2023-12-27 18:50:12 -0500

    agregando cambios en la estructura del archivo sql y tareas pendientes para realizar

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ciudad-tienda
 proyecto de software (30/11/21)
 Este proyecto es solo una práctica en donde aplico todos mis conocimientos obtenidos hasta la fecha.
+
+COSAS GREGAR
+-Agregar un botón para eliminar los productos publicados
+
+-Mejorar la presentación de los productos. Estos deben aportar más información
+
+-Agregar un funcionalidad que permida al usuario cambiar su foto de perfil
+    pulsando la foto de un perfil.
+
+-
--- sql/database.sql
+++ sql/database.sql
@@ -1,26 +1,30 @@
 CREATE schema ciudad;
 use ciudad;
+drop table user;
+drop table producto;
 create table user(
-	id int auto_increment not null,
+	id int(2) auto_increment not null,
     nombre varchar(50) not null,
 	celular varchar(9) unique not null,
-    correo varchar(50) unique not null,
+    email varchar(50) unique not null,
     password varchar(50) not null,
+    seguidores int(3) not null,
     PRIMARY KEY (id)
 );
 create table producto(
 	id int(2) auto_increment not null,
     id_user int(2) not null,
     nombre varchar(50) not null,
-    description text(100) default 'sin description',
-    foto longblob,
+    description text(100) not null,
+    foto longblob not null,
 	unidad_medida varchar(30) not null,
+    cantidad_unidad_medida float(5) not null,
     unidad_precio float(5) not null,
-    precio_por_mayor float(5) default 0,
-    stock int not null default 0,
-    fecha_public TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY (id),
-	FOREIGN KEY (id_user) references user (id)
+    precio_por_mayor float(5) not null,
+    stock int not null,
+    fecha_public TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 );
--- INSERT INTO producto (id_user, nombre, description, foto, unidad_medida, unidad_precio, precio_por_mayor, stock) 
--- VALUE ('', '', '', '', '', '', '', '');
+-- select * from producto where producto.nombre like 'bo%'
+-- INSERT INTO producto (id_user, nombre, description, foto, unidad_medida, cantidad_unidad_medida, unidad_precio, precio_por_mayor, stock) 
+-- VALUE ('', '', '', '', '', '', '', '', '');
